8f9e99b113b176f2f18ddee5746a69697cae134f
Spencer Wang <ziruiwang.spencer@gmail.com>
2024-07-31 13:53:32 -0700

    07/31/2024

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,8 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Home',
   lang: 'en', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 280, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false, // Hide the theme color picker for visitors
+    hue: 80, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
